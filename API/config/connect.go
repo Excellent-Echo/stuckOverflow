7f2ed527c4b366668491deeb586809d4d44f322b
marwanjuna <mrwnolii@gmail.com>
2021-05-31 10:39:33 +0700

    add connect db config

---
+++ API/config/connect.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Excellent-Echo/stuckOverflow/API/API/entity"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func Connect() *gorm.DB {
+	err := godotenv.Load()
+
+	dbUser := os.Getenv("DB_USERNAME")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db.AutoMigrate(&entity.User{})
+	db.AutoMigrate(&entity.Questions{})
+	db.AutoMigrate(&entity.Categories{})
+	db.AutoMigrate(&entity.Answers{})
+	db.AutoMigrate(&entity.UserDetail{})
+
+	return db
+}
