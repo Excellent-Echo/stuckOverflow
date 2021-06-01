996b167e72b59614c50c9417bce6780eddfdf5de
Levi1812 <you@example.com>
2021-06-01 20:26:06 +0700

    AMPUN SUHU

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      </header>
+      <body>
+        <Login />
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Login = () => {
+    <div className="flex-row align-items-center">
+        <div className="Container">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input placeholder="Email" className="form-control"></input>
+                            </div>
+
+                            <div className="form-group">
+                                <label>Password</label>
+                                <input placeholder="Password" className="form-control"></input>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Login
