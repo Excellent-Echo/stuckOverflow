1cbfcb283dd54993d493ef6afdb1488fbb2525eb
marwanjuna <mrwnolii@gmail.com>
2021-06-03 01:56:31 +0700

    modify cors in main.go

--- API/main.go
+++ API/main.go
@@ -4,13 +4,14 @@ import (
 	"os"
 
 	"github.com/Excellent-Echo/stuckOverflow/API/API/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// jobsapi.StoreToDB()
 	r := gin.Default()
-	// r.Use(cors.Default())
+	r.Use(cors.Default())
 
 	routes.UserRoute(r)
 	routes.QuestionRoute(r)
