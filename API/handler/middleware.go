07588316e73167a17285136dd6fb8d42841b813a
marwanjuna <mrwnolii@gmail.com>
2021-06-03 11:51:38 +0700

    add cors middleware

--- API/handler/middleware.go
+++ API/handler/middleware.go
@@ -46,17 +46,16 @@ func Middleware(userService user.UserService, authService auth.Service) gin.Hand
 
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH, UPDATE")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(200)
-		} else {
-			c.Next()
+			return
 		}
+
+		c.Next()
 	}
 }
